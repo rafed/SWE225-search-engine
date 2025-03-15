24267751640a98f4a060a68e305f1e366b14b96b
Rafed Muhammad Yasir <rafed123@gmail.com>
2025-03-14 17:21:51 -0700

    Integrate backend & frontend

--- server.py
+++ server.py
@@ -1,16 +1,25 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 from query_processor import search
+import time
 
 app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 @app.route('/search', methods=['GET'])
 def search_endpoint():
     query = request.args.get('q')
 
+    start_time = time.time()
     results = search(query)
-    return jsonify(results)
+    end_time = time.time()
+
+    return jsonify({
+        'urls': [result[2] for result in results],
+        'time': (end_time - start_time) * 1000 # milliseconds
+    })
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
